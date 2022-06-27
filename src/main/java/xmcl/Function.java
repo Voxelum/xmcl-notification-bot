12baa7c48f1a25153bd7ee5c6e6013a36e7cf80a
CI010 <cijhn@hotmail.com>
2022-06-27 19:54:36 +0800

    Add http to test

--- src/main/java/xmcl/Function.java
+++ src/main/java/xmcl/Function.java
@@ -1,9 +1,14 @@
 package xmcl;
 
 import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.BindingName;
 import com.microsoft.azure.functions.annotation.BlobTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
 import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.BotFactory;
 import net.mamoe.mirai.contact.Group;
@@ -13,24 +18,28 @@ import net.mamoe.mirai.contact.announcement.OfflineAnnouncement;
 import net.mamoe.mirai.contact.announcement.OnlineAnnouncement;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
+    @FunctionName("release-trigger")
+    public String trigger(
+            @HttpTrigger(name = "release-trigger", methods = {
+                    HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request) {
+        return "Hello";
+    }
+
     /**
-     * This function listens at endpoint "/api/release". Two ways to invoke it using "curl" command in bash:
+     * This function listens at endpoint "/api/release". Two ways to invoke it using
+     * "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/release
      * 2. curl "{your host}/api/release?name=HTTP%20Query"
      */
     @FunctionName("release")
     public void run(
-            @BlobTrigger(
-                    name = "version-released",
-                    dataType = "string",
-                    path = "releases/{name}",
-                    connection = "XMCL_STORAGE_CONNECTION")
-                    String content,
+            @BlobTrigger(name = "version-released", dataType = "string", path = "releases/{name}", connection = "XMCL_STORAGE_CONNECTION") String content,
             @BindingName("name") String filename,
             final ExecutionContext context) {
         if (!filename.equals("VERSION")) {
